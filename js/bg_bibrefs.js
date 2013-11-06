b3e65c0fcb96933f7ff2ae69336a4c8558ade0e3
VBog <VBog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 20:48:59 +0000

    Version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/bg-biblie-references/trunk@800046 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/bg_bibrefs.js
+++ js/bg_bibrefs.js
@@ -17,28 +17,28 @@ jQuery('a.bg_data_title').mouseenter(function(e){
 			}
 		});
 	}
-		var position = el.position();
-		var mousex = e.pageX + 2; //Получаем координаты по оси X
-		var mousey =  position.top + el.height(); // Получаем координаты по оси Y
-		var tipWidth = tooltip.width(); //Вычисляем ширину подсказки
-		var tipHeight = tooltip.height(); // Вычисляем высоту подсказки
-		//Определяем дистанцию от правого края окна браузера до блока, содержащего подсказку
-		var tipVisX = jQuery(window).width() - (mousex + tipWidth);
-		// Определяем дистанцию от ниждего края окна браузера до блока, содержащего подсказку        
-		var tipVisY = jQuery(window).height() - (mousey + tipHeight);
+	var position = el.position();
+	var mousex = e.pageX + 2; //Получаем координаты по оси X
+	var mousey =  position.top + el.height(); // Получаем координаты по оси Y
+	var tipWidth = tooltip.width(); //Вычисляем ширину подсказки
+	var tipHeight = tooltip.height(); // Вычисляем высоту подсказки
+	//Определяем дистанцию от правого края окна браузера до блока, содержащего подсказку
+	var tipVisX = jQuery(window).width() - (mousex + tipWidth);
+	// Определяем дистанцию от ниждего края окна браузера до блока, содержащего подсказку        
+	var tipVisY = jQuery(window).height() - (mousey + tipHeight);
 
-		if ( tipVisX < 20 ) { //Если ширина подсказки превышает расстояние от правого края окна браузера до курсора,
-			mousex = e.pageX - 8*tipWidth/10 - 2; // то распологаем область с подсказкой по другую сторону от курсора
-		} 
-		if ( tipVisY < 20 ) { // Если высота подсказки превышает расстояние от нижнего края окна браузера до курсора,
-			tipHeight = parseInt(tooltip.css('max-height'));
-			tooltip.css('height', tipHeight+"px");
-			mousey = position.top - tipHeight;  // то распологаем область с подсказкой над курсором
-		} 
-		mousex = mousex - position.left - 2*tipWidth/10;
-		mousey = mousey - position.top;
-		//Непосредственно присваиваем найденные координаты области, содержащей подсказку
-		tooltip.css('top', mousey+"px");
-		tooltip.css('left', mousex+"px");
+	if ( tipVisX < 20 ) { //Если ширина подсказки превышает расстояние от правого края окна браузера до курсора,
+		mousex = e.pageX - 8*tipWidth/10 - 2; // то распологаем область с подсказкой по другую сторону от курсора
+	} 
+	if ( tipVisY < 20 ) { // Если высота подсказки превышает расстояние от нижнего края окна браузера до курсора,
+		tipHeight = parseInt(tooltip.css('max-height'));
+		tooltip.css('height', tipHeight+"px");
+		mousey = position.top - tipHeight;  // то распологаем область с подсказкой над курсором
+	} 
+	mousex = mousex - position.left - 2*tipWidth/10;
+	mousey = mousey - position.top;
+	//Непосредственно присваиваем найденные координаты области, содержащей подсказку
+	tooltip.css('top', mousey+"px");
+	tooltip.css('left', mousex+"px");
 });
 
