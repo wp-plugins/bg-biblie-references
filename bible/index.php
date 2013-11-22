0cd82dd673df2413cff87afacf37f39438c9ede0
VBog <VBog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-22 06:11:45 +0000

    Version 2.3.5
    git-svn-id: https://plugins.svn.wordpress.org/bg-biblie-references/trunk@808643 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bible/index.php
+++ bible/index.php
@@ -1,4 +1,6 @@
 <?php 
-include ('../includes/quotes.php');
+$wpblogheader = substr(__FILE__, 0, strpos(__FILE__, 'wp-content')).'wp-blog-header.php'; 
+if (!file_exists($wpblogheader))exit($wpblogheader." - not found.");
+include_once ($wpblogheader);
+include_once ('../includes/quotes.php');
 echo bg_bibfers_getQuotes($_GET["title"], $_GET["chapter"], $_GET["type"]);
-
