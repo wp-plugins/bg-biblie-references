d72d0128e8bf7a21621ca2b35d406d4ef34cb724
VBog <VBog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-22 10:17:00 +0000

    Fixer bug.
    git-svn-id: https://plugins.svn.wordpress.org/bg-biblie-references/trunk@808747 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bible/index.php
+++ bible/index.php
@@ -1,6 +1,10 @@
 <?php 
-//$wpblogheader = substr(__FILE__, 0, strpos(__FILE__, 'wp-content')).'wp-blog-header.php'; 
-//if (!file_exists($wpblogheader))exit($wpblogheader." - not found.");
-//include_once ($wpblogheader);
+echo "\n";
+$title = $_GET["title"];
+$chapter = $_GET["chapter"];
+if (!$chapter) $chapter = '1-99';
+$type = $_GET["type"];
+if (!$type) $type = 'verses';
+include_once (realpath (substr(__FILE__, 0, strpos(__FILE__, 'wp-content')).'wp-blog-header.php'));
 include_once ('../includes/quotes.php');
-echo bg_bibfers_getQuotes($_GET["title"], $_GET["chapter"], $_GET["type"]);
+echo bg_bibfers_getQuotes($title, $chapter, $type);
